3324d091ca13e2ff0ce5541b495664d121894c20
kikuuuty <kikuuuty@gmail.com>
2016-09-29 15:16:23 +0900

    Add dither to options

--- ispc_texcomp/processing.cpp
+++ ispc_texcomp/processing.cpp
@@ -16,8 +16,9 @@ const wchar_t kHowToUse[] = {
     _T("    -h, --help      このメッセージです\n")
     _T("    -v, --verbose   詳細表示モード\n")
     _T("    -f <format>     圧縮フォーマット\n")
-    _T("    -l <level>      圧縮レベル(bc6h,bc7)\n")
+    _T("    -l <level>      圧縮レベル(bc6h,bc7のみ)\n")
     _T("    -rgb            アルファチャンネルを無視する(bc7のみ)\n")
+    _T("    -dither         ディザ処理を行う(bc1,bc3のみ)\n")
     _T("\n")
     _T("  <format>\n")
     _T("    bc1,bc3,bc6h,bc7\n")
@@ -149,7 +150,9 @@ void show_usage()
 struct Spec
 {
     bool verbose;
+    bool useDxTex;
     bool rgbMode;
+    bool dither;
     Format format;
     Level level;
     String inputName;
@@ -157,11 +160,125 @@ struct Spec
 
     Spec()
         : verbose(false)
+        , useDxTex(false)
         , rgbMode(false)
+        , dither(false)
         , format(Format::BC3)
         , level(Level::Basic)
     {}
 };
+
+static std::unique_ptr<DirectX::ScratchImage> load_image_from_file(String& name)
+{
+    DirectX::TexMetadata meta;
+    auto image = std::make_unique<DirectX::ScratchImage>();
+    HRESULT hr = DirectX::LoadFromTGAFile(name.c_str(), &meta, *image);
+    if (FAILED(hr)) {
+        hr = DirectX::LoadFromWICFile(name.c_str(), DirectX::WIC_FLAGS_NONE, &meta, *image);
+        if (FAILED(hr)) {
+            printf(_T("  DirectX::LoadFromXXXFile failed. [%s]\n"), name.c_str());
+            return nullptr;
+        }
+    }
+    return image;
+}
+
+static std::unique_ptr<DirectX::ScratchImage> compress_with_dxtex(const std::shared_ptr<Spec> spec)
+{
+    HRESULT hr;
+
+    auto src = load_image_from_file(spec->inputName);
+    auto dst = std::make_unique<DirectX::ScratchImage>();
+
+    DWORD compress = DirectX::TEX_COMPRESS_DEFAULT;
+    if (spec->dither) {
+        compress |= DirectX::TEX_COMPRESS_DITHER;
+    }
+    hr = DirectX::Compress(src->GetImages(), src->GetImageCount(), src->GetMetadata(),
+                           format_to_dxgiformat(spec->format), compress, DirectX::TEX_THRESHOLD_DEFAULT, *dst);
+    if (FAILED(hr)) {
+        printf(_T("  DirectX::Compress failed. ret = 0x%x\n"), hr);
+        return nullptr;
+    }
+    return dst;
+}
+
+static std::unique_ptr<DirectX::ScratchImage> compress_with_ispc(const std::shared_ptr<Spec> spec)
+{
+    HRESULT hr;
+
+    auto image = load_image_from_file(spec->inputName);
+    auto& meta = image->GetMetadata();
+
+    if (!is_block_align(spec->format, meta.width, meta.height)) {
+        printf(_T("  Input width and height need to be a multiple of block size.\n"));
+        printf(_T("  4 bytes/block for BC1/ETC1, 8 bytes/block for BC3/BC6H/BC7/ASTC,\n"));
+        printf(_T("  the blocks are stored in raster scan order (natural CPU texture layout)."));
+        return nullptr;
+    }
+
+    if (meta.format != DXGI_FORMAT_R8G8B8A8_UNORM) {
+        auto temp = std::make_unique<DirectX::ScratchImage>();
+        hr = DirectX::Convert(image->GetImages(), image->GetImageCount(), image->GetMetadata(),
+                              DXGI_FORMAT_R8G8B8A8_UNORM, DirectX::TEX_FILTER_DEFAULT, 0.5f, *temp);
+        if (FAILED(hr)) {
+            printf(_T("  DirectX::Convert failed. ret = 0x%x\n"), hr);
+            return nullptr;
+        }
+        image = std::move(temp);
+    }
+
+    auto width = static_cast<uint32_t>(image->GetMetadata().width);
+    auto height = static_cast<uint32_t>(image->GetMetadata().height);
+
+    DirectX::Blob blob;
+    hr = blob.Initialize(get_memory_size(width, height, spec->format));
+    if (FAILED(hr)) {
+        printf(_T("  DirectX::Blob::Initialize failed. ret = 0x%x\n"), hr);
+        return nullptr;
+    }
+
+    rgba_surface surface;
+    surface.ptr = image->GetPixels();
+    surface.width = width;
+    surface.height = height;
+    surface.stride = width * 4; // format is must be DXGI_FORMAT_R8G8B8A8_UNORM
+
+    switch (spec->format) {
+    case Format::BC1:
+        CompressBlocksBC1(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()));
+        break;
+
+    case Format::BC3:
+        CompressBlocksBC3(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()));
+        break;
+
+    case Format::BC6H:
+        {
+            bc6h_enc_settings setting;
+            init_bc6h_enc_settings(&setting, spec->level);
+            CompressBlocksBC6H(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()), &setting);
+        }
+        break;
+
+    case Format::BC7:
+        {
+            bc7_enc_settings setting;
+            init_bc7_enc_settings(&setting, spec->level, spec->rgbMode);
+            CompressBlocksBC7(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()), &setting);
+        }
+        break;
+    }
+
+    hr = image->Initialize2D(format_to_dxgiformat(spec->format), width, height, 1, 1);
+    if (FAILED(hr)) {
+        printf(_T("  DirectX::ScratchImage::Initialize2D failed. ret = 0x%x\n"), hr);
+        return nullptr;
+    }
+
+    memcpy(image->GetPixels(), blob.GetBufferPointer(), blob.GetBufferSize());
+    return image;
+}
 
 std::shared_ptr<Spec> init_spec_from(const CommandLineOptions& options)
 {
@@ -203,6 +320,10 @@ std::shared_ptr<Spec> init_spec_from(const CommandLineOptions& options)
             else if (key == _T("-rgb")) {
                 spec->rgbMode = true;
             }
+            else if (key == _T("-dither")) {
+                spec->useDxTex = true;
+                spec->dither = true;
+            }
             else if (key == _T("-i")) {
                 if (kv.second.size() >= 1) {
                     spec->inputName = kv.second[0];
@@ -228,96 +349,22 @@ bool compress_and_save(const std::shared_ptr<Spec> spec)
         return false;
     }
 
-    DirectX::TexMetadata meta;
-    auto image = std::make_unique<DirectX::ScratchImage>();
-    hr = DirectX::LoadFromTGAFile(spec->inputName.c_str(), &meta, *image);
-    if (FAILED(hr)) {
-        hr = DirectX::LoadFromWICFile(spec->inputName.c_str(), DirectX::WIC_FLAGS_NONE, &meta, *image);
+    std::unique_ptr<DirectX::ScratchImage> image = spec->useDxTex ? compress_with_dxtex(spec) : compress_with_ispc(spec);
+
+    int res = S_FALSE;
+    if (image) {
+        hr = DirectX::SaveToDDSFile(image->GetImages(), image->GetImageCount(), image->GetMetadata(),
+                                    DirectX::DDS_FLAGS_NONE, spec->outputName.c_str());
         if (FAILED(hr)) {
-            printf(_T("  DirectX::LoadFromXXXFile failed. [%s]\n"), spec->inputName.c_str());
-            return false;
+            printf(_T("  DirectX::SaveToDDSFile failed. ret = 0x%x\n"), hr);
+        }
+        else {
+            printf(_T("done.\n"));
+            res = S_OK;
         }
     }
-
-    if (!is_block_align(spec->format, meta.width, meta.height)) {
-        printf(_T("  Input width and height need to be a multiple of block size.\n"));
-        printf(_T("  4 bytes/block for BC1/ETC1, 8 bytes/block for BC3/BC6H/BC7/ASTC,\n"));
-        printf(_T("  the blocks are stored in raster scan order (natural CPU texture layout)."));
-        return false;
-    }
-
-    if (meta.format != DXGI_FORMAT_R8G8B8A8_UNORM) {
-        auto temp = std::make_unique<DirectX::ScratchImage>();
-        hr = DirectX::Convert(image->GetImages(), image->GetImageCount(), image->GetMetadata(),
-                              DXGI_FORMAT_R8G8B8A8_UNORM, DirectX::TEX_FILTER_DEFAULT, 0.5f, *temp);
-        if (FAILED(hr)) {
-            printf(_T("  DirectX::Convert failed. ret = 0x%x\n"), hr);
-            return false;
-        }
-        image = std::move(temp);
-    }
-
-    uint32_t width = static_cast<uint32_t>(image->GetMetadata().width);
-    uint32_t height = static_cast<uint32_t>(image->GetMetadata().height);
-
-    DirectX::Blob blob;
-    hr = blob.Initialize(get_memory_size(width, height, spec->format));
-    if (FAILED(hr)) {
-        printf(_T("  DirectX::Blob::Initialize failed. ret = 0x%x\n"), hr);
-        return false;
-    }
-
-    rgba_surface surface;
-    surface.ptr = image->GetPixels();
-    surface.width = width;
-    surface.height = height;
-    surface.stride = width * 4; // format is must be DXGI_FORMAT_R8G8B8A8_UNORM
-
-    switch (spec->format) {
-    case Format::BC1:
-        CompressBlocksBC1(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()));
-        break;
-
-    case Format::BC3:
-        CompressBlocksBC3(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()));
-        break;
-
-    case Format::BC6H:
-        {
-            bc6h_enc_settings setting;
-            init_bc6h_enc_settings(&setting, spec->level);
-            CompressBlocksBC6H(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()), &setting);
-        }
-        break;
-
-    case Format::BC7:
-        {
-            bc7_enc_settings setting;
-            init_bc7_enc_settings(&setting, spec->level, spec->rgbMode);
-            CompressBlocksBC7(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()), &setting);
-        }
-        break;
-    }
-
-    hr = image->Initialize2D(format_to_dxgiformat(spec->format), width, height, 1, 1);
-    if (FAILED(hr)) {
-        printf(_T("  DirectX::ScratchImage::Initialize2D failed. ret = 0x%x\n"), hr);
-        return false;
-    }
-
-    memcpy(image->GetPixels(), blob.GetBufferPointer(), blob.GetBufferSize());
-
-    hr = DirectX::SaveToDDSFile(image->GetImages(), image->GetImageCount(), image->GetMetadata(),
-                                DirectX::DDS_FLAGS_NONE, spec->outputName.c_str());
-    if (FAILED(hr)) {
-        printf(_T("  DirectX::SaveToDDSFile failed. ret = 0x%x\n"), hr);
-        return false;
-    }
-
     ::CoUninitialize();
-
-    printf(_T("done.\n"));
-    return true;
+    return res == S_OK ? true : false;
 }
 
 }
