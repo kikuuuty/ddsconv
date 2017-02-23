5a79d6a760103e1428bbc56163834c35b1d44377
kikuuuty <kikuuuty@gmail.com>
2017-02-23 12:26:12 +0900

    Clean up source code

--- ispc_texcomp/processing.cpp
+++ ispc_texcomp/processing.cpp
@@ -28,14 +28,6 @@ const wchar_t kHowToUse[] = {
     _T("\n")
 };
 
-enum class Format
-{
-    BC1,
-    BC3,
-    BC6H,
-    BC7,
-};
-
 const wchar_t* kFormats[] = {
     _T("bc1"),
     _T("bc3"),
@@ -43,14 +35,11 @@ const wchar_t* kFormats[] = {
     _T("bc7"),
 };
 
-enum class Level
-{
-    UltraFast,
-    VeryFast,
-    Fast,
-    Basic,
-    Slow,
-    VerySlow,
+const DXGI_FORMAT kDXGIFormats[] = {
+    DXGI_FORMAT_BC1_UNORM,
+    DXGI_FORMAT_BC3_UNORM,
+    DXGI_FORMAT_BC6H_UF16,
+    DXGI_FORMAT_BC7_UNORM,
 };
 
 const wchar_t* kLevels[] = {
@@ -62,26 +51,15 @@ const wchar_t* kLevels[] = {
     _T("veryslow"),
 };
 
-bool is_block_align(Format format, size_t width, size_t)
+enum class Level
 {
-    size_t block_align = format == Format::BC1 ? 4 : 8;
-    return ISALIGN(width, block_align) /*&& ISALIGN(height, block_align)*/;
-}
-
-size_t get_memory_size(uint32_t width, uint32_t height, Format format)
-{
-    switch (format) {
-    case Format::BC1:
-        return (((width + 3) / 4) * ((height + 3) / 4)) * 8 /** depth*/;
-    case Format::BC3:
-    case Format::BC6H:
-    case Format::BC7:
-        return (((width + 3) / 4) * ((height + 3) / 4)) * 16 /** depth*/;
-    default:
-        break;
-    }
-    return width * height * 4;
-}
+    UltraFast,
+    VeryFast,
+    Fast,
+    Basic,
+    Slow,
+    VerySlow,
+};
 
 void init_bc6h_enc_settings(bc6h_enc_settings* settings, Level level)
 {
@@ -97,9 +75,9 @@ void init_bc6h_enc_settings(bc6h_enc_settings* settings, Level level)
     }
 }
 
-void init_bc7_enc_settings(bc7_enc_settings* settings, Level level, bool alpha_ignored)
+void init_bc7_enc_settings(bc7_enc_settings* settings, Level level, bool rgb_mode)
 {
-    if (alpha_ignored) {
+    if (rgb_mode) {
         switch (level) {
         case Level::UltraFast:  GetProfile_ultrafast(settings); break;
         case Level::VeryFast:   GetProfile_veryfast(settings); break;
@@ -125,17 +103,20 @@ void init_bc7_enc_settings(bc7_enc_settings* settings, Level level, bool alpha_i
     }
 }
 
-DXGI_FORMAT format_to_dxgiformat(Format format)
+size_t compute_image_size(DXGI_FORMAT format, int32_t width, int32_t height)
 {
     switch (format) {
-    case Format::BC1:   return DXGI_FORMAT_BC1_UNORM;
-    case Format::BC3:   return DXGI_FORMAT_BC3_UNORM;
-    case Format::BC6H:  return DXGI_FORMAT_BC6H_UF16;
-    case Format::BC7:
+    case DXGI_FORMAT_BC1_UNORM:
+        return (((width + 3) / 4) * ((height + 3) / 4)) * 8;
+    case DXGI_FORMAT_BC3_UNORM:
+        return (((width + 3) / 4) * ((height + 3) / 4)) * 16;
+    case DXGI_FORMAT_BC6H_UF16:
+    case DXGI_FORMAT_BC7_UNORM:
+        return (((width + 3) / 4) * ((height + 3) / 4)) * 16;
     default:
         break;
     }
-    return DXGI_FORMAT_BC7_UNORM;
+    return 0;
 }
 
 }
@@ -150,25 +131,23 @@ void show_usage()
 struct Spec
 {
     bool verbose;
-    bool useDxTex;
-    bool rgbMode;
+    bool rgb_mode;
     bool dither;
-    Format format;
+    DXGI_FORMAT format;
     Level level;
-    String inputName;
-    String outputName;
+    String input_name;
+    String output_name;
 
     Spec()
         : verbose(false)
-        , useDxTex(false)
-        , rgbMode(false)
+        , rgb_mode(false)
         , dither(false)
-        , format(Format::BC3)
+        , format(DXGI_FORMAT_BC3_UNORM)
         , level(Level::Basic)
     {}
 };
 
-static std::unique_ptr<DirectX::ScratchImage> load_image_from_file(String& name)
+static std::unique_ptr<DirectX::ScratchImage> load_image_from_file(const String& name)
 {
     DirectX::TexMetadata meta;
     auto image = std::make_unique<DirectX::ScratchImage>();
@@ -183,40 +162,12 @@ static std::unique_ptr<DirectX::ScratchImage> load_image_from_file(String& name)
     return image;
 }
 
-static std::unique_ptr<DirectX::ScratchImage> compress_with_dxtex(const std::shared_ptr<Spec> spec)
+static std::unique_ptr<DirectX::ScratchImage> compress_with_ispc(std::unique_ptr<DirectX::ScratchImage> image,
+                                                                 const std::shared_ptr<Spec> spec)
 {
     HRESULT hr;
 
-    auto src = load_image_from_file(spec->inputName);
-    auto dst = std::make_unique<DirectX::ScratchImage>();
-
-    DWORD compress = DirectX::TEX_COMPRESS_DEFAULT;
-    if (spec->dither) {
-        compress |= DirectX::TEX_COMPRESS_DITHER;
-    }
-    hr = DirectX::Compress(src->GetImages(), src->GetImageCount(), src->GetMetadata(),
-                           format_to_dxgiformat(spec->format), compress, DirectX::TEX_THRESHOLD_DEFAULT, *dst);
-    if (FAILED(hr)) {
-        printf(_T("  DirectX::Compress failed. ret = 0x%x\n"), hr);
-        return nullptr;
-    }
-    return dst;
-}
-
-static std::unique_ptr<DirectX::ScratchImage> compress_with_ispc(const std::shared_ptr<Spec> spec)
-{
-    HRESULT hr;
-
-    auto image = load_image_from_file(spec->inputName);
     auto& meta = image->GetMetadata();
-
-    if (!is_block_align(spec->format, meta.width, meta.height)) {
-        printf(_T("  Input width and height need to be a multiple of block size.\n"));
-        printf(_T("  4 bytes/block for BC1/ETC1, 8 bytes/block for BC3/BC6H/BC7/ASTC,\n"));
-        printf(_T("  the blocks are stored in raster scan order (natural CPU texture layout)."));
-        return nullptr;
-    }
-
     if (meta.format != DXGI_FORMAT_R8G8B8A8_UNORM) {
         auto temp = std::make_unique<DirectX::ScratchImage>();
         hr = DirectX::Convert(image->GetImages(), image->GetImageCount(), image->GetMetadata(),
@@ -232,7 +183,7 @@ static std::unique_ptr<DirectX::ScratchImage> compress_with_ispc(const std::shar
     auto height = static_cast<uint32_t>(image->GetMetadata().height);
 
     DirectX::Blob blob;
-    hr = blob.Initialize(get_memory_size(width, height, spec->format));
+    hr = blob.Initialize(compute_image_size(spec->format, width, height));
     if (FAILED(hr)) {
         printf(_T("  DirectX::Blob::Initialize failed. ret = 0x%x\n"), hr);
         return nullptr;
@@ -242,18 +193,18 @@ static std::unique_ptr<DirectX::ScratchImage> compress_with_ispc(const std::shar
     surface.ptr = image->GetPixels();
     surface.width = width;
     surface.height = height;
-    surface.stride = width * 4; // format is must be DXGI_FORMAT_R8G8B8A8_UNORM
+    surface.stride = width * 4; // The format must be DXGI_FORMAT_R8G8B8A8_UNORM.
 
     switch (spec->format) {
-    case Format::BC1:
+    case DXGI_FORMAT_BC1_UNORM:
         CompressBlocksBC1(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()));
         break;
 
-    case Format::BC3:
+    case DXGI_FORMAT_BC3_UNORM:
         CompressBlocksBC3(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()));
         break;
 
-    case Format::BC6H:
+    case DXGI_FORMAT_BC6H_UF16:
         {
             bc6h_enc_settings setting;
             init_bc6h_enc_settings(&setting, spec->level);
@@ -261,16 +212,16 @@ static std::unique_ptr<DirectX::ScratchImage> compress_with_ispc(const std::shar
         }
         break;
 
-    case Format::BC7:
+    case DXGI_FORMAT_BC7_UNORM:
         {
             bc7_enc_settings setting;
-            init_bc7_enc_settings(&setting, spec->level, spec->rgbMode);
+            init_bc7_enc_settings(&setting, spec->level, spec->rgb_mode);
             CompressBlocksBC7(&surface, static_cast<uint8_t*>(blob.GetBufferPointer()), &setting);
         }
         break;
     }
 
-    hr = image->Initialize2D(format_to_dxgiformat(spec->format), width, height, 1, 1);
+    hr = image->Initialize2D(spec->format, width, height, 1, 1);
     if (FAILED(hr)) {
         printf(_T("  DirectX::ScratchImage::Initialize2D failed. ret = 0x%x\n"), hr);
         return nullptr;
@@ -278,6 +229,26 @@ static std::unique_ptr<DirectX::ScratchImage> compress_with_ispc(const std::shar
 
     memcpy(image->GetPixels(), blob.GetBufferPointer(), blob.GetBufferSize());
     return image;
+}
+
+static std::unique_ptr<DirectX::ScratchImage> compress_with_dxtex(std::unique_ptr<DirectX::ScratchImage> image,
+                                                                  const std::shared_ptr<Spec> spec)
+{
+    HRESULT hr;
+
+    DWORD compress = DirectX::TEX_COMPRESS_DEFAULT;
+    if (spec->dither) {
+        compress |= DirectX::TEX_COMPRESS_DITHER;
+    }
+
+    auto dst = std::make_unique<DirectX::ScratchImage>();
+    hr = DirectX::Compress(image->GetImages(), image->GetImageCount(), image->GetMetadata(),
+                           spec->format, compress, DirectX::TEX_THRESHOLD_DEFAULT, *dst);
+    if (FAILED(hr)) {
+        printf(_T("  DirectX::Compress failed. ret = 0x%x\n"), hr);
+        return nullptr;
+    }
+    return dst;
 }
 
 std::shared_ptr<Spec> init_spec_from(const CommandLineOptions& options)
@@ -298,8 +269,8 @@ std::shared_ptr<Spec> init_spec_from(const CommandLineOptions& options)
                 if (args.size() >= 1) {
                     auto& format = args[0];
                     for (int32_t i = 0; i < ARRAYSIZE(kFormats); ++i) {
-                        if ( format == kFormats[i] ) {
-                            spec->format = static_cast<Format>(i);
+                        if (format == kFormats[i]) {
+                            spec->format = kDXGIFormats[i];
                             break;
                         }
                     }
@@ -318,25 +289,31 @@ std::shared_ptr<Spec> init_spec_from(const CommandLineOptions& options)
                 }
             }
             else if (key == _T("-rgb")) {
-                spec->rgbMode = true;
+                spec->rgb_mode = true;
             }
             else if (key == _T("-dither")) {
-                spec->useDxTex = true;
                 spec->dither = true;
             }
             else if (key == _T("-i")) {
                 if (kv.second.size() >= 1) {
-                    spec->inputName = kv.second[0];
+                    spec->input_name = kv.second[0];
                 }
             }
             else if (key == _T("-o")) {
                 if (kv.second.size() >= 1) {
-                    spec->outputName = kv.second[0];
+                    spec->output_name = kv.second[0];
                 }
             }
         }
     }
     return spec;
+}
+
+inline bool is_dither_compression(const std::shared_ptr<Spec> spec)
+{
+    if (spec->format != DXGI_FORMAT_BC1_UNORM && spec->format != DXGI_FORMAT_BC3_UNORM) return false;
+    if (!spec->dither) return false;
+    return true;
 }
 
 bool compress_and_save(const std::shared_ptr<Spec> spec)
@@ -349,12 +326,24 @@ bool compress_and_save(const std::shared_ptr<Spec> spec)
         return false;
     }
 
-    std::unique_ptr<DirectX::ScratchImage> image = spec->useDxTex ? compress_with_dxtex(spec) : compress_with_ispc(spec);
+    auto image = load_image_from_file(spec->input_name);
+    if (!image) {
+        return false;
+    }
+
+    bool with_ispc_texcomp = true;
+    auto& meta = image->GetMetadata();
+    if (!ISALIGN(meta.width, 4) || !ISALIGN(meta.height, 4) || is_dither_compression(spec)) {
+        with_ispc_texcomp = false;
+    }
+
+    image = with_ispc_texcomp ? compress_with_ispc(std::move(image), spec) :
+                                compress_with_dxtex(std::move(image), spec);
 
     int res = S_FALSE;
     if (image) {
         hr = DirectX::SaveToDDSFile(image->GetImages(), image->GetImageCount(), image->GetMetadata(),
-                                    DirectX::DDS_FLAGS_NONE, spec->outputName.c_str());
+                                    DirectX::DDS_FLAGS_NONE, spec->output_name.c_str());
         if (FAILED(hr)) {
             printf(_T("  DirectX::SaveToDDSFile failed. ret = 0x%x\n"), hr);
         }
